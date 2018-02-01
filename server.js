c6a74669377be82f4527a4ff56b39f702ff0cfb8
Philip Flohr <philip.flohr@student.kit.edu>
2018-02-01 11:27:47 +0100

    add the vpn ip4 address to a capturing group

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ const updateServer = (server) => {
   const IPv6 = "(?:[\[])(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(?:[\]])"
   const IPv4_or_IPv6 = "(" + IPv6 + "|" + IPv4 + ")"
   const IPv4_or_IPv6_with_port = IPv4_or_IPv6 + ":[0-9]{1,5}"
-  var regex = new RegExp("^CLIENT_LIST,([a-zA-Z]+)," + IPv4_or_IPv6_with_port + "," + IPv4 + ",.*,.*,.*,(.*),.*,.*,.*,.*$");
+  var regex = new RegExp("^CLIENT_LIST,([a-zA-Z]+)," + IPv4_or_IPv6_with_port + ",(" + IPv4 + "),.*,.*,.*,(.*),.*,.*,.*,.*$");
+  console.log(regex)
   const content = fs.readFileSync(server.logFile, 'utf8').trim().split('\n')
   const rawEntries = content.map((line) => line.match(regex)).filter((itm) => itm)
   const entries = rawEntries.map((entry) => ({
