afcdc38f3242f180f194d56c1ce15492c55f5af2
Chris Vaas <chrisvaas@gmail.com>
2018-02-05 16:52:41 +0000

    Added hook script

---
+++ status.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$script_type" == "client-connect" ]; then
+  JSON="{\"pub\":\"$trusted_ip\",\"cn\":\"$common_name\",\"vpn\":\"$ifconfig_pool_remote_ip\"}"
+  curl -H "Content-Type: application/json" -X POST -d "$JSON" http://localhost:3013/server/0/connect --connect-timeout 2 -m 3
+elif [ "$script_type" == "client-disconnect" ]; then
+  JSON="{\"cn\":\"$common_name\"}"
+  curl -H "Content-Type: application/json" -X POST -d "$JSON" http://localhost:3013/server/0/disconnect --connect-timeout 2 -m 3
+fi
+exit 0
