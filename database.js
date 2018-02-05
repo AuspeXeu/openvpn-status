161cee716413985c8091660560108630eda1b165
Chris Vaas <chrisvaas@gmail.com>
2018-02-05 18:06:33 +0000

    Simplified database interface and added columns

--- database.js
+++ database.js
@@ -1,38 +1,33 @@
 const Sequelize = require('sequelize')
 
-const init = () => {
-  return new Promise ((resolve) => {
-    const sequelize = new Sequelize('status', 'server', '!$%openvpn1', {
-      dialect: 'sqlite',
-      storage: './data.sqlite',
-      logging: false,
-      pool: {max: 5, min: 0, idle: 10000}
-    })
+const sequelize = new Sequelize('status', 'server', '!$%openvpn1', {
+  dialect: 'sqlite',
+  storage: './data.sqlite',
+  logging: false,
+  pool: {max: 5, min: 0, idle: 10000}
+})
 
-    const Log = sequelize.define('Log', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      timestamp : {type: Sequelize.INTEGER},
-      server    : {type: Sequelize.INTEGER},
-      node      : {type: Sequelize.STRING},
-      event     : {type: Sequelize.STRING}
-    },{
-      freezeTableName: true,
-      timestamps: false,
-      tableName: 'Log'
-    })
-
-    module.exports.Log = Log
-    module.exports.state = () => sequelize.query('select p1.* from log as p1 inner join (select max(timestamp) as timestamp, node from log group by node) as p2 on p1.node = p2.node and p1.timestamp = p2.timestamp where p1.event = \'connect\'', {model:Log})
-
-    Log.sync().then(resolve)
-  })
-}
+const Log = sequelize.define('Log', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  timestamp : {type: Sequelize.INTEGER},
+  server    : {type: Sequelize.INTEGER},
+  pub       : {type: Sequelize.STRING},
+  vpn       : {type: Sequelize.STRING},
+  node      : {type: Sequelize.STRING},
+  event     : {type: Sequelize.STRING}
+},{
+  freezeTableName: true,
+  timestamps: false,
+  tableName: 'Log'
+})
 
 module.exports = {
-  init: init,
+  init: () => Log.sync(),
+  Log: Log,
+  state: () => sequelize.query('select p1.* from log as p1 inner join (select max(timestamp) as timestamp, node from log group by node) as p2 on p1.node = p2.node and p1.timestamp = p2.timestamp where p1.event = \'connect\'', {model:Log}),
   op: Sequelize.Op
 }
