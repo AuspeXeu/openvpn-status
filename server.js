0511faaca34ba47c60ac255977b3e83d6c5b734a
Chris Vaas <chrisvaas@gmail.com>
2018-03-12 19:16:16 +0000

    Input validation

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ const logEvent = (server, data, event) => {
   }
 }
 const validateIPaddress = (ipaddress) => /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress.toString())
+const validateNumber = (n) => !isNaN(parseFloat(n)) && isFinite(n)
 const loadIPdatabase = () => {
   return new Promise((resolve) => {
     fs.stat(ipFile, (err, stat) => {
@@ -107,39 +108,44 @@ const compare = (a,b) => {
     return -1
   return 0
 }
-app.post('/server/:id/connect', (req, res) => {
+const validateServer = (req, res, next) => {
+  if (!validateNumber(req.params.id))
+    return res.sendStatus(400)
+  if (!servers[req.params.id])
+    return res.sendStatus(404)
+  next()
+}
+app.post('/server/:id/connect', validateServer, (req, res) => {
   const serverId = parseInt(req.params.id, 10)
   const pub = req.body.pub
   const cn = (req.body.cn || '').trim()
   const vpn = req.body.vpn
   if (!validateIPaddress(pub) || !validateIPaddress(vpn) || !cn.length)
-    res.sendStatus(400)
-  else {
-    const loc = cityLookup.get(pub)
-    const entry = {name: cn, pub: pub, vpn: vpn, timestamp: moment().unix()}
-    if (loc) {
-      entry.country_code = loc.country.iso_code
-      entry.country_name = loc.country.names.en
-    }
-    logEvent(serverId, entry, 'connect')
-    servers[serverId].entries.push(entry)
-    res.sendStatus(200)
+    return res.sendStatus(400)
+  const loc = cityLookup.get(pub)
+  const entry = {name: cn, pub: pub, vpn: vpn, timestamp: moment().unix()}
+  if (loc) {
+    entry.country_code = loc.country.iso_code
+    entry.country_name = loc.country.names.en
   }
+  logEvent(serverId, entry, 'connect')
+  servers[serverId].entries.push(entry)
+  res.sendStatus(200)
 })
-app.post('/server/:id/disconnect', (req, res) => {
+app.post('/server/:id/disconnect', validateServer, (req, res) => {
   const serverId = parseInt(req.params.id, 10)
   const cn = (req.body.cn || '').trim()
   if (!cn.length)
-    res.sendStatus(400)
-  else {
-    logEvent(serverId, {name: cn}, 'disconnect')
-    servers[serverId].entries = servers[serverId].entries.filter((itm) => itm.name !== cn)
-    res.sendStatus(200)
-  }
+    return res.sendStatus(400)
+  logEvent(serverId, {name: cn}, 'disconnect')
+  servers[serverId].entries = servers[serverId].entries.filter((itm) => itm.name !== cn)
+  res.sendStatus(200)
 })
-app.get('/entries/:id', (req, res) => res.json(servers[req.params.id].entries.sort(compare)))
-app.get('/log/:id/size', (req, res) => db.Log.count({where: {server: req.params.id}}).then((size) => res.json({value: size})))
-app.get('/log/:id/:page/:size', (req, res) => {
+app.get('/entries/:id', validateServer, (req, res) => res.json(servers[req.params.id].entries.sort(compare)))
+app.get('/log/:id/size', validateServer, (req, res) => db.Log.count({where: {server: req.params.id}}).then((size) => res.json({value: size})))
+app.get('/log/:id/:page/:size', validateServer, (req, res) => {
+  if (!validateNumber(req.params.page) || !validateNumber(req.params.size))
+    return res.sendStatus(400)
   const page = parseInt(req.params.page, 10)
   const size = parseInt(req.params.size, 10)
   const query = db.Log.findAll({where: {server: req.params.id}, offset: (page - 1) * size, limit: size, order: [['timestamp', 'DESC']]})
