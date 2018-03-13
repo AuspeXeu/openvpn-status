aa53f2e0132f9846b54899376a30404d75010fc9
Chris Vaas <chrisvaas@gmail.com>
2018-03-13 11:33:38 +0000

    Simplified script trigger

--- server.js
+++ server.js
@@ -101,30 +101,31 @@ const validateServer = (req, res, next) => {
     return res.sendStatus(404)
   next()
 }
-app.post('/server/:id/connect', validateServer, (req, res) => {
+app.post('/server/:id/script', validateServer, (req, res) => {
   const serverId = parseInt(req.params.id, 10)
   const pub = req.body.pub
   const cn = (req.body.cn || '').trim()
   const vpn = req.body.vpn
-  if (!validateIPaddress(pub) || !validateIPaddress(vpn) || !cn.length)
-    return res.sendStatus(400)
-  const loc = cityLookup.get(pub)
-  const entry = {name: cn, pub: pub, vpn: vpn, timestamp: moment().unix()}
-  if (loc) {
-    entry.country_code = loc.country.iso_code
-    entry.country_name = loc.country.names.en
-  }
-  logEvent(serverId, entry, 'connect')
-  servers[serverId].entries.push(entry)
-  res.sendStatus(200)
-})
-app.post('/server/:id/disconnect', validateServer, (req, res) => {
-  const serverId = parseInt(req.params.id, 10)
-  const cn = (req.body.cn || '').trim()
+  const script = req.body.script.trim()
+
   if (!cn.length)
     return res.sendStatus(400)
-  logEvent(serverId, {name: cn}, 'disconnect')
-  servers[serverId].entries = servers[serverId].entries.filter((itm) => itm.name !== cn)
+
+  if (script === 'client-connect') {
+    if (!validateIPaddress(pub) || !validateIPaddress(vpn))
+      return res.sendStatus(400)
+    const loc = cityLookup.get(pub)
+    const entry = {name: cn, pub: pub, vpn: vpn, timestamp: moment().unix()}
+    if (loc) {
+      entry.country_code = loc.country.iso_code
+      entry.country_name = loc.country.names.en
+    }
+    logEvent(serverId, entry, 'connect')
+    servers[serverId].entries.push(entry)
+  } else if (script === 'client-disconnect') {
+    logEvent(serverId, {name: cn}, 'disconnect')
+    servers[serverId].entries = servers[serverId].entries.filter((itm) => itm.name !== cn)
+  }
   res.sendStatus(200)
 })
 app.get('/entries/:id', validateServer, (req, res) => res.json(servers[req.params.id].entries))
--- status.sh
+++ status.sh
@@ -3,11 +3,6 @@ HOST="127.0.0.1"
 PORT="3013"
 SERVER="0"
 
-if [ "$script_type" == "client-connect" ]; then
-  JSON="{\"pub\":\"$trusted_ip\",\"cn\":\"$common_name\",\"vpn\":\"$ifconfig_pool_remote_ip\"}"
-  curl -H "Content-Type: application/json" -X POST -d "$JSON" "http://$HOST:$PORT/server/$SERVER/connect" --connect-timeout 2 -m 3
-elif [ "$script_type" == "client-disconnect" ]; then
-  JSON="{\"cn\":\"$common_name\"}"
-  curl -H "Content-Type: application/json" -X POST -d "$JSON" "http://$HOST:$PORT/server/$SERVER/disconnect" --connect-timeout 2 -m 3
-fi
+JSON="{\"script\":\"$script_type\",\"pub\":\"$trusted_ip\",\"cn\":\"$common_name\",\"vpn\":\"$ifconfig_pool_remote_ip\"}"
+curl -H "Content-Type: application/json" -X POST -d "$JSON" "http://$HOST:$PORT/server/$SERVER/script" --connect-timeout 2 -m 3
 exit 0
