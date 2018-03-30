9c9fae63bb44dd220f798c6a9ffa58064d99a24c
Wout <woutwoot13@gmail.com>
2018-03-30 13:39:50 +0200

    Fix missing comma in JSON.
    This comma was missing and produced a JSON parsing error. I've added it.

--- status.sh
+++ status.sh
@@ -3,6 +3,6 @@ HOST="127.0.0.1"
 PORT="3013"
 SERVER="0"
 
-JSON="{\"script\":\"$script_type\",\"pub\":\"$trusted_ip\",\"cn\":\"$common_name\"\"user\":\"$username\",\"vpn\":\"$ifconfig_pool_remote_ip\"}"
+JSON="{\"script\":\"$script_type\",\"pub\":\"$trusted_ip\",\"cn\":\"$common_name\",\"user\":\"$username\",\"vpn\":\"$ifconfig_pool_remote_ip\"}"
 curl -H "Content-Type: application/json" -X POST -d "$JSON" "http://$HOST:$PORT/server/$SERVER/script" --connect-timeout 2 -m 3
 exit 0
