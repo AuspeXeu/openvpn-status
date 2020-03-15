43ad4e497ee385db988bf4d0240566289c6e0886
Chris Vaas <christianvaas@auspex.eu>
2020-03-15 15:44:38 -0400

    feat(servers): merge from cfg and servers folder

--- utils.js
+++ utils.js
@@ -1,15 +1,14 @@
 const moment = require('moment')
+const fs = require('fs')
 const maxmind = require('maxmind')
 const conf = require('nconf')
 const path = require('path')
 
-conf.file({file: path.join(__dirname, 'cfg.json')})
+conf.file({file: path.resolve(__dirname, './cfg.json')})
 conf.defaults({
   port: process.env.STATUS_PORT || 3013,
   bind: process.env.STATUS_BIND || '127.0.0.1',
-  servers: [{
-    name: 'Server', host: '127.0.0.1', man_port: 7656
-  }],
+  servers: [],
   username: process.env.STATUS_USERNAME || 'admin',
   password: process.env.STATUS_PASSWORD || 'admin',
   web: {
@@ -17,11 +16,16 @@ conf.defaults({
   }
 })
 
+const servers = fs.readdirSync(path.resolve(__dirname, './servers'))
+  .filter((fname) => fname.endsWith('.json'))
+  .map((fname) => JSON.parse(fs.readFileSync(path.resolve(__dirname, `./servers/${fname}`), 'utf8')))
+conf.set('servers', [...conf.get('servers'), ...servers])
+
 const log = (...args) => console.log(...[moment().format(conf.get('web').dateFormat), ...args])
 
 const loadIPdatabase = () => {
   return new Promise((resolve, reject) => {
-    maxmind.open(path.join(__dirname, 'GeoLite2-City.mmdb'))
+    maxmind.open(path.resolve(__dirname, 'GeoLite2-City.mmdb'))
       .then((lookup) => resolve(ip => (ip ? lookup.get(ip) : false)))
       .catch((err) => {reject();log(err)})
   })
