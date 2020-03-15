e25dd80d86336e2e7248090c4b30a98e8626efed
Chris Vaas <christianvaas@auspex.eu>
2020-03-15 13:50:31 -0400

    fix: allow ENV vars to control cfg

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM node:12
 
+ENV STATUS_BIND="0.0.0.0"
+ENV STATUS_PORT="3013"
+
 WORKDIR /usr/src/app
 COPY . .
 RUN npm install
--- utils.js
+++ utils.js
@@ -5,15 +5,15 @@ const path = require('path')
 
 conf.file({file: path.join(__dirname, 'cfg.json')})
 conf.defaults({
-  port: 3013,
-  bind: '127.0.0.1',
+  port: process.env.STATUS_PORT || 3013,
+  bind: process.env.STATUS_BIND || '127.0.0.1',
   servers: [{
     name: 'Server', host: '127.0.0.1', man_port: 7656
   }],
-  username: 'admin',
-  password: 'admin',
+  username: process.env.STATUS_USERNAME || 'admin',
+  password: process.env.STATUS_PASSWORD || 'admin',
   web: {
-    dateFormat: 'HH:mm:ss - DD.MM.YY'
+    dateFormat: process.env.STATUS_WEB_FORMAT || 'HH:mm:ss - DD.MM.YY'
   }
 })
 
